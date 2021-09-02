1157a6c5321762c678beb1fb489e59fa57bfb990
Arvid Gustafson <arvidg@iastate.edu>
2021-09-01 21:43:09 -0500

    Made progress on a tutorial.
    I am following along within an experiment. Currently, I have a landing
    activity with buttons that disable themselves when pressed. Furthermore,
    it has no errors nor warnings.

---
+++ Experiments/Arvid/Tutorial/app/src/main/java/com/example/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn = (Button) findViewById(R.id.button1);
+        btn.setOnClickListener(this::disable);
+    }
+
+    public void disable(View v) {
+        View myView = findViewById(R.id.button1);
+        myView.setEnabled(false);
+        Button button = (Button) myView;
+        button.setText(R.string.New_Disabled);
+    }
+
+}
