28792153b4140487a7ddcaa8e2f88b1b09bdf08b
mjreecy <mjreecy@iastate.edu>
2021-09-05 10:39:06 -0500

    Update MainActivity.java, changing settings to work with java programming.

--- Experiments/Marcus/MainActivity.java
+++ Experiments/Marcus/MainActivity.java
@@ -5,15 +5,16 @@ import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+public class MainActivity extends AppCompatActivity() {
+    
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
         Spinner firstspinner = (Spinner) findViewByID(R.id.spinner2);
         ArrayAdapter<String> newadapts = new ArrayAdapter<String>(MainActivity.this,
         android.R.layout.simple_list_item_1, getResources().getStringArray(R.getStringArray(R.array.Foods));
 
         newadapts.setDropdownViewResource(android.R.layout.simple_dropdown_item_1line);
-        spinner2.setAdapter.(myAdapter);
-    }
+        spinner2.setAdapter.(newadapts);
+    
 }
