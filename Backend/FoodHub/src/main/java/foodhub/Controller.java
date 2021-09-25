68ce99d2b940a008231160f9997547b03bf156aa
Arvid Gustafson <arvidg@iastate.edu>
2021-09-24 21:11:02 -0500

    Prepared the springboot project to be exported.
    Prepared the springboot project so that it could be exported, and run on
    the server. Also, I altered the package names.

--- Backend/FoodHub/src/main/java/foodhub/Controller.java
+++ Backend/FoodHub/src/main/java/foodhub/Controller.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package foodhub;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +8,7 @@ public class Controller {
 
     @GetMapping("/")
     public String welcome() {
-        return "Hello, World!";
+        return "Hello, World!!!";
     }
 
 }
