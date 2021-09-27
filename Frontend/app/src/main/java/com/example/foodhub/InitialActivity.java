a5b37ce1f457cdd0e51dadb2e4dbcca70dd48b38
Kimara <ekimara@Kimaras-MacBook-Air.local>
2021-09-26 22:18:39 -0700

    added a login page

--- Frontend/app/src/main/java/com/example/foodhub/InitialActivity.java
+++ Frontend/app/src/main/java/com/example/foodhub/InitialActivity.java
@@ -14,10 +14,15 @@ public class InitialActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button btn = findViewById(R.id.customer_sign_up_button);
-        btn.setOnClickListener(this::goToSignInActivity);
+        btn.setOnClickListener(this::goToSignUpActivity);
     }
 
     public void goToSignInActivity(View v) {
+        Intent i = new Intent(this, LoginActivity.class);
+        startActivity(i);
+    }
+
+    public void goToSignUpActivity(View v) {
         Intent i = new Intent(this, SignUpActivity.class);
         startActivity(i);
     }
