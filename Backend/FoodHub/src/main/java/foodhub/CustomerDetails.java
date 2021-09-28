e898979f2d7f3faabb2daf739d95063b67a69379
jamiea1 <jamiea1@iastate.edu>
2021-09-28 01:57:49 -0500

    Added CustomerDetails class, mostly overrides of security methods

---
+++ Backend/FoodHub/src/main/java/foodhub/CustomerDetails.java
@@ -0,0 +1,59 @@
+package foodhub;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class CustomerDetails implements UserDetails{
+
+	private String email;
+	private String password;
+	
+	public CustomerDetails(Customer customer) {
+		this.email = customer.getEmail();
+		this.password = customer.getPassword();		
+	}
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return new HashSet<GrantedAuthority>();
+	}
+
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return password;
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+}
