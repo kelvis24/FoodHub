ca5229ac1a906f5138798faa761ea9bd4efdd425
kelvis24 <ekimara@iastate.edu>
2021-10-01 02:05:25 -0700

    finished writing code but doesnt work

--- Frontend/app/src/main/java/com/example/foodhub/HomeActivity.java
+++ Frontend/app/src/main/java/com/example/foodhub/HomeActivity.java
@@ -1,6 +1,9 @@
 package com.example.foodhub;
 
+import static com.example.foodhub.R.id.recyclerView;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
@@ -9,22 +12,28 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class HomeActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
+    private ArrayList<Company> arrayList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        arrayList= new ArrayList<>();
+        recyclerView = findViewById(R.id.recyclerView);
 
-        recyclerView = findViewById(R.id.recyckerView);
-
-        recyclerView.setAdapter();
+        arrayList.add( new Company(R.drawable.ic_launcher_background, R.drawable.litramen, "title", "message"));
+        RecyclerAdapter recyclerAdapter = new RecyclerAdapter(arrayList);
+        recyclerView.setAdapter(recyclerAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         Intent I = getIntent();
         String name = I.getStringExtra("Email");
-        ((TextView)findViewById(R.id.homepage_username)).setText(name);
+      //  ((TextView)findViewById(R.id.homepage_username)).setText(name);
 
     }
 }
--- Frontend/app/src/main/java/com/example/foodhub/RecyclerAdapter.java
+++ Frontend/app/src/main/java/com/example/foodhub/RecyclerAdapter.java
@@ -1,43 +1,67 @@
 package com.example.foodhub;
 
+import android.media.Image;
 import android.view.InflateException;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder> {
 
-    //Arra
-    public RecyclerAdapter() {
-
+    private ArrayList<Company> arrayList;
+    public RecyclerAdapter(ArrayList<Company> arrayList) {
+    this.arrayList  = arrayList;
     }
+
+
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
-        inflater.inflate();
-        return null;
+        View view = inflater.inflate(R.layout.company_view, parent,false);
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
 
+        Company company = arrayList.get(position);
+        holder.title.setText((company.getTitle()));
+        holder.message.setText((company.getMessage()));
+        holder.profileImage.setImageResource((company.getProfileIcon()));
+        holder.postImage.setImageResource((company.getPostImage()));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return arrayList.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
 
+        ImageView profileImage;
+        ImageView postImage;
+        TextView title;
+        TextView message;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-        }
+
+            profileImage = itemView.findViewById(R.id.ivProfile);
+            postImage = itemView.findViewById(R.id.ivPost);
+            title = itemView.findViewById(R.id.title);
+            message = itemView.findViewById(R.id.message);
+
+
+       }
     }
 
 
