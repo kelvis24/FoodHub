6b7090273bc413d5d9cd779a57fb2f57d898a656
Arvid Gustafson <arvidg@iastate.edu>
2021-10-03 14:21:45 -0500

    Added Checks for Password on Signup With Marcus

--- Frontend/app/src/main/java/com/example/foodhub/SignUpActivity.java
+++ Frontend/app/src/main/java/com/example/foodhub/SignUpActivity.java
@@ -1,5 +1,7 @@
 package com.example.foodhub;
 
+import java.util.regex.*;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -28,11 +30,56 @@ public class SignUpActivity extends AppCompatActivity {
         String cPassword = ((EditText)findViewById(R.id.sign_up_confirm_password_field)).getText().toString();
         String location = ((EditText)findViewById(R.id.sign_up_location_field)).getText().toString();
 
+        if (!hasBetween(password, 'a', 'z')) {
+            Toast.makeText(getApplicationContext(),"Password Must Have A Lower Case Letter.",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (!hasBetween(password, 'A', 'Z')) {
+            Toast.makeText(getApplicationContext(),"Password Must Have A Upper Case Letter.",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (!hasBetween(password, '0', '9')) {
+            Toast.makeText(getApplicationContext(),"Password Must Have A Number.",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (!hasSymbol(password)) {
+            Toast.makeText(getApplicationContext(),"Password Must Have A Symbol.",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (password.length() < 12) {
+            Toast.makeText(getApplicationContext(),"Password must have at least 12 characters.",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (!password.equals(cPassword)) {
+            Toast.makeText(getApplicationContext(),"Passwords Do Not Match",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
         I.putExtra("Name", name);
         I.putExtra("Email", email);
         I.putExtra("Location", location);
 
         startActivity(I);
+    }
+
+    public boolean hasBetween(String str, char start, char end) {
+        int i;
+        for (i = 0; i < str.length(); i++) {
+            if (start <= str.charAt(i) && str.charAt(i) <= end)
+                return true;
+        }
+        return false;
+    }
+
+    public boolean hasSymbol(String str) {
+        int i;
+        for (i = 0; i < str.length(); i++) {
+            if (!('a' <= str.charAt(i) && str.charAt(i) <= 'z') &&
+                !('A' <= str.charAt(i) && str.charAt(i) <= 'Z') &&
+                !('0' <= str.charAt(i) && str.charAt(i) <= '9'))
+                return true;
+        }
+        return false;
     }
 
 }
