be874708014a3d38af6833933059f05ddcf0dc05
kelvis24 <ekimara@iastate.edu>
2021-10-07 03:09:12 -0700

    finally got it to work at 3am

--- Frontend/app/src/main/java/com/example/foodhub/HomeActivity.java
+++ Frontend/app/src/main/java/com/example/foodhub/HomeActivity.java
@@ -20,18 +20,22 @@ import android.widget.TextView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.ArrayList;
+import com.example.foodhub.databinding.ActivityHomeBinding;
 
 public class HomeActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
     private ArrayList<Company> arrayList;
+    private ActivityHomeBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        // setContentView(R.layout.activity_home);
 
-
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
 
         BottomNavigationView navView = findViewById(R.id.bottomNavigationView);
@@ -39,9 +43,10 @@ public class HomeActivity extends AppCompatActivity {
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.home2, R.id.pickup, R.id.search, R.id.orders, R.id.account)
                 .build();
+
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
+        NavigationUI.setupWithNavController(binding.bottomNavigationView, navController);
 
         arrayList= new ArrayList<>();
         recyclerView = findViewById(R.id.recyclerView);
@@ -59,9 +64,7 @@ public class HomeActivity extends AppCompatActivity {
 
         Intent I = getIntent();
         String name = I.getStringExtra("Email");
-
-
-
-
     }
+    
+    
 }
