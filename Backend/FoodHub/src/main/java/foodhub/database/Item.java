f9e740275b1ae9cd7c1b9872f8943b6355d4045c
jamiea1 <jamiea1@iastate.edu>
2021-10-11 14:56:30 -0500

    added order repo and entity

--- Backend/FoodHub/src/main/java/foodhub/database/Item.java
+++ Backend/FoodHub/src/main/java/foodhub/database/Item.java
@@ -1,10 +1,14 @@
 package foodhub.database;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="items")
 public class Item {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
