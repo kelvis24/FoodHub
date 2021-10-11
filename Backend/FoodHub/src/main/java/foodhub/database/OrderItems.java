e064d45e825c1e837fe91615acb4f6cbb4e0fa61
jamiea1 <jamiea1@iastate.edu>
2021-10-11 15:00:53 -0500

    added orderitems entity and repository

---
+++ Backend/FoodHub/src/main/java/foodhub/database/OrderItems.java
@@ -0,0 +1,80 @@
+package foodhub.database;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Order_Items")
+public class OrderItems {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	@Column(nullable = false)
+	private long orderId;
+	@Column(nullable = false)
+	private long itemId;
+	@Column(nullable = false)
+	private int quantity;
+	@Column()
+	private String notes;
+	
+	public OrderItems(long id, long orderId, long itemId, int quantity, String notes) {
+		this.id = id;
+		this.orderId = orderId;
+		this.itemId = itemId;
+		this.quantity = quantity;
+		this.notes = notes;
+	}
+
+	public OrderItems() {}
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public long getOrderId() {
+		return orderId;
+	}
+
+	public void setOrderId(long orderId) {
+		this.orderId = orderId;
+	}
+
+	public long getItemId() {
+		return itemId;
+	}
+
+	public void setItemId(long itemId) {
+		this.itemId = itemId;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public String getNotes() {
+		return notes;
+	}
+
+	public void setNotes(String notes) {
+		this.notes = notes;
+	}
+
+	@Override
+	public String toString() {
+		return "OrderItems [id=" + id + ", orderId=" + orderId + ", itemId=" + itemId + ", quantity=" + quantity
+				+ ", notes=" + notes + "]";
+	}
+}
