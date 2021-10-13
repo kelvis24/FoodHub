99bf0224dbba58c1bdcf4200089861ba469a784f
Arvid Gustafson <arvidg@iastate.edu>
2021-10-13 17:49:15 -0500

    Expanded current test and made a BeforeEach method

--- Backend/FoodHub/src/test/java/foodhub/CustomerControllerTests.java
+++ Backend/FoodHub/src/test/java/foodhub/CustomerControllerTests.java
@@ -1,6 +1,7 @@
 package foodhub;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.Assert.assertEquals;
@@ -13,7 +14,6 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-import org.junit.Before;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
@@ -35,12 +35,8 @@ public class CustomerControllerTests {
 
 	private List<Customer> l = new ArrayList<Customer>();
 	
-	@Before
+	@BeforeEach
 	public void init() {
-	}
-	
-	@Test
-	public void createCustomerTest1() {
 		when(customerRepository.findAll()).thenReturn(l);
 		when(customerRepository.findByUsername((String)any(String.class)))
 		.thenAnswer(x-> {
@@ -59,22 +55,46 @@ public class CustomerControllerTests {
 			l.add(c);
 			return null;
 		});
+	}
+	
+	@Test
+	public void createCustomerTest1() {
 		String response;
 		Customer c1 = new Customer("Andrew","andrew@gmail.com","andrew123","andrew blv");
 		Customer c2 = new Customer("John","john@gmail.com","john123","john st");
 		Customer c3 = new Customer("Marry","marry@gmail.com","mary123","marry dr");
+		Customer c4 = new Customer("John II","john@gmail.com","mary123","marry dr");
 		response = cc.createCustomer(c1);
 		assertEquals(success, response);
 		response = cc.createCustomer(c2);
 		assertEquals(success, response);
 		response = cc.createCustomer(c3);
 		assertEquals(success, response);
+		response = cc.createCustomer(c4);
+		assertEquals(failure, response);
 		List<Customer> list = cc.listCustomers();
 		assertEquals(3, list.size());
 		assertEquals("Andrew", list.get(0).getName());
 		assertEquals("andrew@gmail.com", list.get(0).getUsername());
 		assertEquals("andrew123", list.get(0).getPassword());
 		assertEquals("andrew blv", list.get(0).getLocation());
+		assertEquals("John", list.get(1).getName());
+		assertEquals("john@gmail.com", list.get(1).getUsername());
+		assertEquals("john123", list.get(1).getPassword());
+		assertEquals("john st", list.get(1).getLocation());
+		assertEquals("Marry", list.get(2).getName());
+		assertEquals("marry@gmail.com", list.get(2).getUsername());
+		assertEquals("mary123", list.get(2).getPassword());
+		assertEquals("marry dr", list.get(2).getLocation());
+		verify(customerRepository, times(1)).save(c1);
+		verify(customerRepository, times(1)).save(c2);
+		verify(customerRepository, times(1)).save(c3);
+		verify(customerRepository, times(3)).save((Customer)any(Customer.class));
+		verify(customerRepository, times(1)).findByUsername("andrew@gmail.com");
+		verify(customerRepository, times(2)).findByUsername("john@gmail.com");
+		verify(customerRepository, times(1)).findByUsername("marry@gmail.com");
+		verify(customerRepository, times(4)).findByUsername((String)any(String.class));
+		verify(customerRepository, times(1)).findAll();
 	}
 	
 }
