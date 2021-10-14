7146efff2b3e09192f1fa3503807c96cc5ef05fc
kelvis24 <ekimara@iastate.edu>
2021-10-13 21:08:30 -0700

    present homepage for now

--- Frontend/app/src/main/java/com/example/foodhub/LoginActivity.java
+++ Frontend/app/src/main/java/com/example/foodhub/LoginActivity.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -20,11 +19,8 @@ public class LoginActivity extends AppCompatActivity {
     public void login(View v) {
         Button btn = (Button) findViewById(R.id.login_button);
         Intent I = new Intent(this, HomeActivity.class);
-
         String email = ((EditText)findViewById(R.id.login_email_address)).getText().toString();
-
         I.putExtra("Email", email);
-
         startActivity(I);
     }
 }
