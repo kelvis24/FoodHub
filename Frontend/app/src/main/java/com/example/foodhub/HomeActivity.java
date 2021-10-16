3f6c0e6e82aea8bade8feabdbfd714d773af02e3
kelvis24 <ekimara@iastate.edu>
2021-10-16 15:50:04 -0700

    worked on the merging the homepage with macus

--- Frontend/app/src/main/java/com/example/foodhub/HomeActivity.java
+++ Frontend/app/src/main/java/com/example/foodhub/HomeActivity.java
@@ -1,8 +1,5 @@
 package com.example.foodhub;
 
-import static com.example.foodhub.R.id.bottomNavigationView;
-import static com.example.foodhub.R.id.recyclerView;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -11,16 +8,12 @@ import androidx.navigation.ui.NavigationUI;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.ArrayList;
 import com.example.foodhub.databinding.ActivityHomeBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -31,14 +24,14 @@ public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        // setContentView(R.layout.activity_home);
+       super.onCreate(savedInstanceState);
+     //   setContentView(R.layout.activity_home);
 
         binding = ActivityHomeBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
+//
         BottomNavigationView navView = findViewById(R.id.bottomNavigationView);
-
+//
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.home2, R.id.pickup, R.id.search, R.id.orders, R.id.account)
                 .build();
@@ -47,15 +40,13 @@ public class HomeActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.bottomNavigationView, navController);
 
-        LoadCompanies();
+       LoadCompanies();
 
-        Intent I = getIntent();
-        String name = I.getStringExtra("Email");
     }
     
     public void LoadCompanies() {
         arrayList= new ArrayList<>();
-        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView = findViewById(R.id.recyclerView1);
 
         arrayList.add( new Company(R.drawable.ic_launcher_background, R.drawable.litramen, "title", "This is a message"));
         arrayList.add( new Company(R.drawable.ic_launcher_background, R.drawable.litramen, "titles", "C'est un message"));
@@ -69,19 +60,5 @@ public class HomeActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this)); 
     }
 
-    public void LoadCompany(View v) {
-        arrayList= new ArrayList<>();
-        recyclerView = findViewById(R.id.recyclerView);
 
-        arrayList.add( new Company(R.drawable.ic_launcher_background, R.drawable.litramen, "title", "This is a message"));
-        arrayList.add( new Company(R.drawable.ic_launcher_background, R.drawable.litramen, "titles", "C'est un message"));
-        arrayList.add( new Company(R.drawable.ic_launcher_background, R.drawable.litramen, "tit", "enni tie lok"));
-        arrayList.add( new Company(R.drawable.ic_launcher_background, R.drawable.litramen, "dfd", "messages appear here"));
-        arrayList.add( new Company(R.drawable.ic_launcher_background, R.drawable.litramen, "titdfle", "still writing here"));
-
-
-        RecyclerAdapter recyclerAdapter = new RecyclerAdapter(arrayList);
-        recyclerView.setAdapter(recyclerAdapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-    }
 }
--- Frontend/app/src/main/java/com/example/foodhub/LoginActivity.java
+++ Frontend/app/src/main/java/com/example/foodhub/LoginActivity.java
@@ -40,49 +40,50 @@ public class LoginActivity extends AppCompatActivity {
         String password = ((EditText)findViewById(R.id.login_password)).getText().toString();
 
         I.putExtra("Email", email);
+        startActivity(I);
 
-        String tag_json_obj = "json_obj_req";
-
-        String url = Const.URL + "/customers-login";
-
-        ProgressDialog pDialog = new ProgressDialog(this);
-        pDialog.setMessage("Loading...");
-        pDialog.show();
-
-        Map<String, String> postParams = new HashMap<String, String>();
-        postParams.put("username", email);
-        postParams.put("password", password);
-
-        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.POST,
-            url, new JSONObject(postParams), new Response.Listener<JSONObject>() {
-                @Override
-                public void onResponse(JSONObject response) {
-                    Log.d("response", response.toString());
-                    pDialog.hide();
-                    /*
-                    if(response.get("message").equals("failure")){
-                        Toast.makeText(getApplicationContext(),"Email or password not correct, please try again.",Toast.LENGTH_SHORT).show();
-                        return;
-                    }
-                     */
-                    try {if (response.get("message").equals("success"))
-                        startActivity(I);
-                    } catch (Exception e) {Log.d("response", e.toString());}
-                }
-            }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    VolleyLog.d("response", "Error: " + error.getMessage());
-                    pDialog.hide();
-                }
-        }) {
-                @Override
-                public Map<String, String> getHeaders() throws AuthFailureError {
-                    HashMap<String, String> headers = new HashMap<String, String>();
-                    headers.put("Content-Type", "application/json;charset=utf-8");
-                    return headers;
-                }
-        };
-        AppController.getInstance().addToRequestQueue(jsonObjReq, tag_json_obj);
+//        String tag_json_obj = "json_obj_req";
+//
+//        String url = Const.URL + "/customers-login";
+//
+//        ProgressDialog pDialog = new ProgressDialog(this);
+//        pDialog.setMessage("Loading...");
+//        pDialog.show();
+//
+//        Map<String, String> postParams = new HashMap<String, String>();
+//        postParams.put("username", email);
+//        postParams.put("password", password);
+//
+//        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.POST,
+//            url, new JSONObject(postParams), new Response.Listener<JSONObject>() {
+//                @Override
+//                public void onResponse(JSONObject response) {
+//                    Log.d("response", response.toString());
+//                    pDialog.hide();
+//                    /*
+//                    if(response.get("message").equals("failure")){
+//                        Toast.makeText(getApplicationContext(),"Email or password not correct, please try again.",Toast.LENGTH_SHORT).show();
+//                        return;
+//                    }
+//                     */
+//                    try {if (response.get("message").equals("success"))
+//                        startActivity(I);
+//                    } catch (Exception e) {Log.d("response", e.toString());}
+//                }
+//            }, new Response.ErrorListener() {
+//                @Override
+//                public void onErrorResponse(VolleyError error) {
+//                    VolleyLog.d("response", "Error: " + error.getMessage());
+//                    pDialog.hide();
+//                }
+//        }) {
+//                @Override
+//                public Map<String, String> getHeaders() throws AuthFailureError {
+//                    HashMap<String, String> headers = new HashMap<String, String>();
+//                    headers.put("Content-Type", "application/json;charset=utf-8");
+//                    return headers;
+//                }
+//        };
+//        AppController.getInstance().addToRequestQueue(jsonObjReq, tag_json_obj);
     }
 }
