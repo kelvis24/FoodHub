4ba5d4cbe394692ec17a98248164ecba9628f81c
jamiea1 <jamiea1@iastate.edu>
2021-11-01 18:57:41 -0500

    Added basic CustomerController JavaDoc comments

--- Backend/FoodHub/src/main/java/foodhub/controllers/CustomerController.java
+++ Backend/FoodHub/src/main/java/foodhub/controllers/CustomerController.java
@@ -11,6 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 import foodhub.database.*;
 import foodhub.ioObjects.*;
 
+/**
+ * The controller for all Customer backend methods
+ * @author 1_CW_2
+ *
+ */
 @RestController
 public class CustomerController {
 
@@ -29,6 +34,14 @@ public class CustomerController {
 	@Autowired
 	OrderItemRepository orderItemRepository;
 	
+	/**
+	 * Authenticates a Customers login.
+	 * This method always returns a message, either a success on successful authentication
+	 * or a failure with error on failure. Errors include wrong username and wrong password.
+	 * @param body an Authentication entity, includes Customer username and password.
+	 * @return a Message with status of authentication (success/failure with error)
+	 * @see Authentication
+	 */
 	@PostMapping("/customers-authenticate")
 	public Message authenticateCustomer(@RequestBody Authentication body) {
     	Customer user = customerRepository.findByUsername(body.getUsername());
@@ -39,6 +52,15 @@ public class CustomerController {
     	return new Message("success");
 	}
     
+	/**
+	 * Allows a Customer to edit their own information. 
+	 * This method always returns a Message after being called, either a success after successfully editing 
+	 * the customer information or a failure with error on failure. Errors include wrong username, wrong password,
+	 * no customer edits made (null Customer), or the new username already taken in the Customer repository. 
+	 * @param body an EditCustomerInput, which includes Customer login and new Customer information
+	 * @return a Message describing method result (success/failure with error).
+	 * @see EditCustomerInput
+	 */
     @PostMapping("customers-edit-customer")
     public Message createCustomer(@RequestBody EditCustomerInput body) {
     	Customer user = customerRepository.findByUsername(body.getUsername());
@@ -56,6 +78,14 @@ public class CustomerController {
     	return new Message("success");
     }
     
+    /**
+     * Allows a Customer to remove themselves from the repository.
+     * This method always returns a Message, either stating the success of removal or a failure with 
+     * error of why the Customer could not be removed. Errors include wrong username and wrong password. 
+     * @param body an Authentication entity, which needs the Customers login details
+     * @return a Message stating the result of the method (success/failure with error)
+     * @see Authentication
+     */
     @PostMapping("customers-remove-customer")
     public Message removeCustomer(@RequestBody Authentication body) {
     	Customer user = customerRepository.findByUsername(body.getUsername());
@@ -69,6 +99,14 @@ public class CustomerController {
     	return new Message("success");
     }
     
+    /**
+     * Shows a Customer all their current orders.
+     * This method always returns a List of OrderOutputs. This list can be null, and will
+     * be null if the body param is not successfully filled with correct information.
+     * @param body an Authentication entity, which includes Customer login details
+     * @return a List of OrderOutputs for the authorized Customer. 
+     * @see Authentication
+     */
     @PostMapping("/customers-get-orders")
     public List<OrderOutput> getOrders(@RequestBody Authentication body) {
     	List<OrderOutput> output = new ArrayList<OrderOutput>();
@@ -89,6 +127,16 @@ public class CustomerController {
     	return output;
     }
     
+    /**
+     * Allows a Customer to create a new order to a Firm.
+     * This method always returns a message, either after successfully creating a new order in the order
+     * repository, or a failure with an error explaining why this could not be done. Errors include wrong username,
+     * wrong password, no data being given (null), the selected firm not existing in the firm repository (null), or no such
+     * item existing within the item repository (null).
+     * @param body an AddOrderInput entity, which includes Customer login details and Order details
+     * @return a Message detailing the status of the method (success/failure with error)
+     * @see AddOrderInput
+     */
     @PostMapping("/customers-create-order")
     public Message customerOrders(@RequestBody AddOrderInput body) {
     	Customer customer = customerRepository.findByUsername(body.getUsername());
@@ -119,6 +167,14 @@ public class CustomerController {
     	return new Message("success");
     }
     
+    /**
+     * Allows a Customer to remove an Order from the Order repository.
+     * This method always returns a Message after invoction, either a success after successful implementation
+     * or a failure with error explaining why it failed. Errors include wrong username, wrong password, or the order
+     * status being currently pending.
+     * @param body a RemoveEntity entity, including Customer login information and Order information
+     * @return a Message detailing the status of the method (success/failure with error)
+     */
     @PostMapping("customers-remove-order")
     public Message removeOrder(@RequestBody RemoveEntity body) {
     	Customer customer = customerRepository.findByUsername(body.getUsername());
@@ -133,6 +189,10 @@ public class CustomerController {
     	return new Message("success");
     }
     
+    /**
+     * Deletes an Order from the repository based on its ID
+     * @param id the ID of the order to be deleted
+     */
     private void deleteOrder(long id) {
     	orderRepository.deleteById(id);
     	List<OrderItem> list = orderItemRepository.findByOrderId(id);
