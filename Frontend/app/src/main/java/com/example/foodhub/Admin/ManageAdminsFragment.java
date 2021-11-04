6600dc136e50d1015248bcbc9010628074861a0f
Arvid Gustafson <arvidg@iastate.edu>
2021-11-03 19:53:42 -0500

    Gave username/password info to fragments

--- Frontend/app/src/main/java/com/example/foodhub/Admin/ManageAdminsFragment.java
+++ Frontend/app/src/main/java/com/example/foodhub/Admin/ManageAdminsFragment.java
@@ -12,16 +12,33 @@ import com.example.foodhub.R;
 
 public class ManageAdminsFragment extends Fragment {
 
-    public ManageAdminsFragment() {}
+    private String username;
+    private String password;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            Bundle bundle = getArguments();
+            username = bundle.getString("username");
+            password = bundle.getString("password");
+        }
+        System.out.println(username);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_manage_admins, container, false);
+        View view = inflater.inflate(R.layout.fragment_manage_admins, container, false);
+        /*
+        if (getArguments() != null) {
+            Bundle bundle = getArguments();
+            username = bundle.getString("username");
+            password = bundle.getString("password");
+        }
+
+        */
+        System.out.println(username);
+        return view;
     }
 
 }
--- Frontend/app/src/main/java/com/example/foodhub/Admin/OwnerMainActivity.java
+++ Frontend/app/src/main/java/com/example/foodhub/Admin/OwnerMainActivity.java
@@ -1,7 +1,10 @@
 package com.example.foodhub.Admin;
 
+import android.content.ClipData;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -11,25 +14,38 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.example.foodhub.R;
 import com.example.foodhub.databinding.ActivityOwnerMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
 
 public class OwnerMainActivity extends AppCompatActivity {
 
-    private String username;
-    private String password;
+    private Bundle bundle;
+    private NavController navController;
     private ActivityOwnerMainBinding binding;
+    private BottomNavigationItemView manageAdmins;
+    private BottomNavigationItemView manageFirms;
 
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityOwnerMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        AppBarConfiguration config = new AppBarConfiguration.Builder(
-                R.id.manage_admins, R.id.manage_firms).build();
-        NavController navController = Navigation.findNavController(this, R.id.owner_fragment_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, config);
-        NavigationUI.setupWithNavController(binding.ownerBottomNavigationView, navController);
         Intent P = getIntent();
-        username = P.getStringExtra("username");
-        password = P.getStringExtra("password");
+        bundle = P.getExtras();
+        navController = Navigation.findNavController(this, R.id.owner_fragment_main);
+        manageAdmins = findViewById(R.id.manage_admins_option);
+        manageAdmins.setOnClickListener(this::manageAdmins);
+        manageFirms = findViewById(R.id.manage_firms_option);
+        manageFirms.setOnClickListener(this::manageFirms);
+        manageAdmins(manageAdmins);
+    }
+
+    public void manageAdmins(View view) {
+        // manageAdmins.setChecked(true);
+        navController.navigate(R.id.ManageAdmins, bundle);
+    }
+
+    public void manageFirms(View view) {
+        // manageFirms.setChecked(true);
+        navController.navigate(R.id.ManageFirms, bundle);
     }
 
 }
