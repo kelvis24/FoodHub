ab15b78934c2841bb19229ef11b14902242c4397
Arvid Gustafson <arvidg@iastate.edu>
2021-11-04 23:40:20 -0500

    "Account" tab allows a customer to logout

--- Frontend/app/src/main/java/com/example/foodhub/Customer/CustomerAccountFragment.java
+++ Frontend/app/src/main/java/com/example/foodhub/Customer/CustomerAccountFragment.java
@@ -1,9 +1,33 @@
 package com.example.foodhub.Customer;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
 import androidx.fragment.app.Fragment;
+
+import com.example.foodhub.Login.InitialActivity;
+import com.example.foodhub.R;
 
 public class CustomerAccountFragment extends Fragment {
 
+    @Override public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
+    @Override public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle saveInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_customer_account, container, false);
+        Button btn = view.findViewById(R.id.customer_logout_button);
+        btn.setOnClickListener(this::goToSignIn);
+        return view;
+    }
+
+    public void goToSignIn(View view) {
+        Intent I = new Intent(getContext(), InitialActivity.class);
+        startActivity(I);
+    }
 
 }
