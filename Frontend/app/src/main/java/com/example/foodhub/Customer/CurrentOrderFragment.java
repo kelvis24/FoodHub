d556e17935b1fbca51adf69bc9292ff009a55a2a
Arvid Gustafson <arvidg@iastate.edu>
2021-11-05 04:10:29 -0500

    A customer can now submit an order (to the server)

--- Frontend/app/src/main/java/com/example/foodhub/Customer/CurrentOrderFragment.java
+++ Frontend/app/src/main/java/com/example/foodhub/Customer/CurrentOrderFragment.java
@@ -1,6 +1,7 @@
 package com.example.foodhub.Customer;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,9 +14,17 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.foodhub.Common.ItemReference;
 import com.example.foodhub.Common.ItemReferenceAdapter;
+import com.example.foodhub.Firm.ManageCategoriesFragment;
 import com.example.foodhub.R;
+import com.example.foodhub.server.Call;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CurrentOrderFragment extends Fragment {
 
@@ -47,6 +56,8 @@ public class CurrentOrderFragment extends Fragment {
         this.container = container;
         Button btn = view.findViewById(R.id.current_order_back_button);
         btn.setOnClickListener(this::goBack);
+        btn = view.findViewById(R.id.current_order_submit_button);
+        btn.setOnClickListener(this::submitRequest);
         RecyclerView recyclerView = view.findViewById(R.id.current_order_recycler);
         recyclerView.setAdapter(new ItemReferenceAdapter(this, order));
         recyclerView.setLayoutManager(new LinearLayoutManager(container.getContext()));
@@ -60,6 +71,36 @@ public class CurrentOrderFragment extends Fragment {
         else
             ft.replace(R.id.customer_fragment_main, new BrowseCategoriesFragment(firmId, username, password, order));
         ft.commit();
+    }
+
+    public void submitRequest(View view) {
+        Map<String, String> map = new HashMap<>();
+        map.put("username", username);
+        map.put("password", password);
+        JSONObject obj = new JSONObject(map);
+        try{JSONArray arr = new JSONArray();
+            for(int i = 0; i < order.size(); i++) {
+                Map<String, String> notesMap = new HashMap();
+                notesMap.put("notes", order.get(i).getNotes());
+                JSONObject itemObj = new JSONObject(map);
+                itemObj.put("itemId", order.get(i).getItem().getId());
+                itemObj.put("quantity", order.get(i).getQuantity());
+                arr.put(itemObj);
+            }
+            JSONObject dataObj = new JSONObject();
+            dataObj.put("firmId", firmId);
+            dataObj.put("orderList", arr);
+            obj.put("data", dataObj);
+        } catch (JSONException e) {e.printStackTrace();}
+        Call.post("customers-create-order", obj, this::submitResponse, null);
+    }
+
+    public void submitResponse(JSONObject response) {
+        try{if (response.get("message").equals("success")) {
+            final FragmentTransaction ft = getFragmentManager().beginTransaction();
+            ft.replace(R.id.customer_fragment_main, new BrowseFirmsFragment(username, password));
+            ft.commit();
+        }} catch (Exception e) {Log.d("response", e.toString());}
     }
 
 }
