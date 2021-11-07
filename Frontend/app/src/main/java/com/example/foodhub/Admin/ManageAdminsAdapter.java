da01c12d17ec01b162b598176ef4456355f13d6b
mjreecy <mjreecy@iastate.edu>
2021-11-07 17:28:29 -0600

    Added javadoc comments for frontend

--- Frontend/app/src/main/java/com/example/foodhub/Admin/ManageAdminsAdapter.java
+++ Frontend/app/src/main/java/com/example/foodhub/Admin/ManageAdminsAdapter.java
@@ -24,6 +24,12 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+/** 
+ * The class responsible allowing admins to be managed.
+ * @author 1_CW_2
+*/
+
+
 public class ManageAdminsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
     private String username;
@@ -32,6 +38,14 @@ public class ManageAdminsAdapter extends RecyclerView.Adapter<RecyclerView.ViewH
     private ManageAdminsFragment fragment;
 
     private ArrayList<Admin> admins;
+    /**
+    * Constructor for managing admins
+    * @param username Users Username
+    * @param password Users Password
+    * @param order ArrayList of the orders
+    * @param fragment BrowseAdminsFragment
+    * @param admins ArrayList of the admins
+    */
 
     public ManageAdminsAdapter(String username, String password,
             ManageAdminsFragment fragment, ArrayList<Admin> admins) {
@@ -40,27 +54,46 @@ public class ManageAdminsAdapter extends RecyclerView.Adapter<RecyclerView.ViewH
         this.fragment = fragment;
         this.admins = admins;
     }
-
+    /** 
+    * Creates a recycle view on the creation of the method
+    * @param viewType Int to designate the type of view
+    * @param parent A ViewGroup that allows the program to remember the last layout
+    * @return AdminHolder(view)
+    * @see AdminHolder
+    */
     @NonNull @Override public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.view_edit_admin, parent, false);
         return new AdminHolder(view);
     }
-
+    /** 
+    * Instigates the recycled view
+    * @param index Int to designate the type of view
+    * @param holder A ViewGroup that allows the program to remember the last layout
+    */
     @Override public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int index) {
         AdminHolder adminHolder = (AdminHolder) holder;
         adminHolder.usernameText.setText(admins.get(index).getUsername());
         DeleteAdmin response = new DeleteAdmin(admins.get(index).getId(), fragment);
         adminHolder.deleteButton.setOnClickListener(response);
     }
-
+    /** 
+    * Method to get the amin item view type
+    * @param index Int to check the view type
+    * @return admin view type
+    */
     @Override public int getItemViewType(int index) {
         return admins.get(index) == null ? -1 : 0;
     }
-
+    /** 
+    * Method to get the admin count
+    * @return admin size
+    */
     @Override public int getItemCount() {
         return admins.size();
     }
-
+    /** 
+    * Class designated to browse through/manage the given admins
+    */
     class AdminHolder extends RecyclerView.ViewHolder {
         TextView usernameText;
         Button deleteButton;
@@ -70,16 +103,24 @@ public class ManageAdminsAdapter extends RecyclerView.Adapter<RecyclerView.ViewH
             deleteButton = view.findViewById(R.id.edit_admin_button);
         }
     }
-
+    /** 
+    * Class designated to delete an admin
+    */
     class DeleteAdmin implements View.OnClickListener, ObjectResponse {
         private long id;
         private ManageAdminsFragment fragment;
-
+        /** 
+        * Method for getting the deleted admin information
+        * @param id ID of the admin to be deleted
+        * @fragment ManageAdminsFragment constructor
+        */
         public DeleteAdmin(long id, ManageAdminsFragment fragment) {
             this.id = id;
             this.fragment = fragment;
         }
-
+        /**
+        * Method to activate class on click
+        */
         public void onClick(View v) {
             Map<String, String> map = new HashMap<>();
             map.put("username", username);
