7ffe07a9cfbf8056831cde6146e8b7d5785378e3
mjreecy <mjreecy@iastate.edu>
2021-11-07 19:33:15 -0600

    Added javadoc comments to Frontend

--- Frontend/app/src/main/java/com/example/foodhub/Common/Order.java
+++ Frontend/app/src/main/java/com/example/foodhub/Common/Order.java
@@ -5,7 +5,10 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
-
+/**
+* Class responsible for creating and setting up the 'order' type
+* @author 1_CW_1
+*/
 public class Order {
 
     private long id;
@@ -15,7 +18,10 @@ public class Order {
     private int status;
     private double total;
     private ArrayList<ItemReference> list;
-
+    /**
+    * Method constructor to set up an order in response to a JSONObject request
+    * @param obj JSONObject from the JSONObject request
+    */
     public Order(JSONObject obj) throws JSONException {
         this.id = obj.getLong("id");
         this.firm = (String)obj.get("firm");
@@ -29,31 +35,52 @@ public class Order {
             list.add(new ItemReference(arr.getJSONObject(i)));
         }
     }
-
+    /**
+    * Method to return the ID of the order when requested.
+    * @return id
+    */
     public long getId() {
         return id;
     }
-
+    /**
+    * Method to return the firm of the order when requested.
+    * @return firm
+    */
     public String getFirm() {
         return firm;
     }
-
+    /**
+    * Method to return the customer of the order when requested.
+    * @return customer
+    */
     public String getCustomer() {
         return customer;
     }
-
+    /**
+    * Method to return the location of the order when requested.
+    * @return location
+    */
     public String getLocation() {
         return location;
     }
-
+    /**
+    * Method to return the status of the order when requested.
+    * @return status
+    */
     public int getStatus() {
         return status;
-    }
-
+    }   
+    /**
+    * Method to return the total of the order when requested.
+    * @return total
+    */
     public double getTotal() {
         return total;
     }
-
+    /**
+    * Method to return the list of item references of the order when requested.
+    * @return id
+    */
     public ArrayList<ItemReference> getList() {
         return list;
     }
