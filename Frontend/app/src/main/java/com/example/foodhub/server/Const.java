7313ccb1d630ce84403c13dc1ccae0e09f2ebbfd
Arvid Gustafson <arvidg@iastate.edu>
2021-11-08 14:11:43 -0600

    Changed back the URL on the frontend

--- Frontend/app/src/main/java/com/example/foodhub/server/Const.java
+++ Frontend/app/src/main/java/com/example/foodhub/server/Const.java
@@ -9,10 +9,10 @@ public class Const {
     /**
      * A URL for a local backend for testing; swap names for a quick switch.
      */
-    public static final String URL = "http://10.0.2.2:3000/";
+    public static final String OTHER_URL = "http://10.0.2.2:3000/";
 
     /**
      * A URL for a remote backend for deployment; swap names for a quick switch.
      */
-    public static final String OURL = "http://coms-309-002.cs.iastate.edu:3000/";
+    public static final String URL = "http://coms-309-002.cs.iastate.edu:3000/";
 }
