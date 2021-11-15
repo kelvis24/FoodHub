5ac0a73907d95a64d5d7140c86b6637a30e79913
Arvid Gustafson <arvidg@iastate.edu>
2021-11-14 20:57:25 -0600

    Added a WebSocketConfig class

---
+++ Backend/FoodHub/src/main/java/foodhub/WebSocketConfig.java
@@ -0,0 +1,23 @@
+package foodhub;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ *
+ * What happens here is that the server end point -- in this case it is
+ * the web socket end point handler is registered with SPRING
+ * so that requests to ws:// will be honored.
+ *
+ * @author Vamsi Krishna Calpakkam
+ *
+ */
+@Configuration
+public class WebSocketConfig {
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter(){
+        return new ServerEndpointExporter();
+    }
+}
+
