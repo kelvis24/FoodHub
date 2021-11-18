dd9e3bd1275fa0658b4f1850edc0bc544a333208
mjreecy <mjreecy@iastate.edu>
2021-11-18 12:00:13 -0600

    Pushing base set

---
+++ Frontend/app/src/main/java/com/example/foodhub/Admin/ViewFirmAdapter.java
@@ -0,0 +1,80 @@
+package com.example.foodhub.Admin;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.foodhub.Common.Category;
+import com.example.foodhub.Common.ItemReference;
+import com.example.foodhub.R;
+
+import java.util.ArrayList;
+
+public class ViewFirmAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private long firmId;
+    private String username;
+    private String password;
+
+    private ViewFirmFragment fragment;
+
+    private ArrayList<Category> categories;
+
+    public ViewFirmAdapter(long firmId, String username, String password, ArrayList<ItemReference> order,
+                           ViewFirmFragment fragment) {
+        this.firmId = firmId;
+        this.username = username;
+        this.password = password;
+        this.fragment = fragment;
+    }
+
+    @NonNull @Override public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.view_browse_category, parent, false);
+        return new CategoryHolder(view);
+    }
+
+    @Override public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int index) {
+        CategoryHolder categoryHolder = (CategoryHolder) holder;
+        categoryHolder.usernameText.setText(categories.get(index).getTitle());
+        GoToBrowseItems goToBrowseItems = new GoToBrowseItems(categories.get(index).getId(), fragment);
+        categoryHolder.usernameText.setOnClickListener(goToBrowseItems);
+    }
+
+    @Override public int getItemViewType(int index) {
+        return categories.get(index) == null ? -1 : 0;
+    }
+
+    @Override public int getItemCount() {
+        return categories.size();
+    }
+
+    class CategoryHolder extends RecyclerView.ViewHolder {
+        TextView usernameText;
+        public CategoryHolder(@NonNull View view) {
+            super(view);
+            usernameText = view.findViewById(R.id.browse_category_textview);
+        }
+    }
+
+    class GoToBrowseItems implements View.OnClickListener {
+        private long categoryId;
+        private ViewFirmFragment fragment;
+
+        public GoToBrowseItems(long categoryId, ViewFirmFragment fragment) {
+            this.categoryId = categoryId;
+            this.fragment = fragment;
+        }
+
+        public void onClick(View v) {
+            final FragmentTransaction ft = fragment.getFragmentManager().beginTransaction();
+            ft.replace(R.id.customer_fragment_main, new ViewFirmFragment(firmId, username, password));
+            ft.commit();
+        }
+    }
+
+}
