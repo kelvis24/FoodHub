73485d1c7ccd6f3470a462a712e8e1af8cf9acf5
kelvis24 <ekimara@iastate.edu>
2021-11-19 21:16:10 -0600

    got the new fragment to take in orders correctly

--- Frontend/app/src/main/java/com/example/foodhub/Customer/BrowseSpecificFirmOrders.java
+++ Frontend/app/src/main/java/com/example/foodhub/Customer/BrowseSpecificFirmOrders.java
@@ -3,6 +3,8 @@ package com.example.foodhub.Customer;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,8 +13,15 @@ import android.view.ViewGroup;
 import com.example.foodhub.Common.Firm;
 import com.example.foodhub.Common.Order;
 import com.example.foodhub.R;
+import com.example.foodhub.server.Call;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -44,7 +53,39 @@ public class BrowseSpecificFirmOrders extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_browse_specific_firm_orders, container, false);
+        this.container = container;
+        refresh();
+        return view;
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_browse_specific_firm_orders, container, false);
+     //   return inflater.inflate(R.layout.fragment_browse_specific_firm_orders, container, false);
+    }
+
+    public void refresh() {
+        Map<String, String> map = new HashMap<>();
+        map.put("username", username);
+        map.put("password", password);
+        JSONObject obj = new JSONObject(map);
+
+        Call.post("customers-get-orders", obj, this::listOrders, null);
+
+    }
+
+
+    /**
+     * Lists order information upon a successful call to refresh the page
+     * @param arr The response from the server as a JSONArray
+     */
+    public void listOrders(JSONArray arr) {
+        orders = new ArrayList<>();
+        for (int i = 0; i < arr.length(); i++) {
+            try{orders.add(new Order(arr.getJSONObject(i)));
+            } catch (JSONException e) {e.printStackTrace();}
+        }
+
+        RecyclerView recyclerView = container.findViewById(R.id.see_specific_orders_to_firm);
+        recyclerView.setAdapter(new ManageCustomerOrdersAdapter(username, password, this, orders));
+        recyclerView.setLayoutManager(new LinearLayoutManager(container.getContext()));
     }
 }
