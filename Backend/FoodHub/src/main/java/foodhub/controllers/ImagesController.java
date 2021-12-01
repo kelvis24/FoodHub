971cfe266459381153229f75386cf9f78bb8746e
vskja <vskja@LAPTOP-0NT35953.lan>
2021-11-30 21:33:30 -0600

    gave each of the repos an upload and a download

--- Backend/FoodHub/src/main/java/foodhub/controllers/ImagesController.java
+++ Backend/FoodHub/src/main/java/foodhub/controllers/ImagesController.java
@@ -18,7 +18,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import foodhub.ImageStorageService;
-import foodhub.database.Images;
+import foodhub.database.CategoryImage;
+import foodhub.database.FirmImage;
+import foodhub.database.ItemImage;
+import foodhub.ioObjects.Authentication;
 
 
 @Controller
@@ -27,28 +30,57 @@ public class ImagesController {
 	@Autowired 
 	private ImageStorageService imageStorageService;
 	
-	@GetMapping("/")
-	public String get(Model model) {
-		List<Images> upload = imageStorageService.getFiles();
-		model.addAttribute("images", upload);
-		return "doc";
-	}
-	
-	@PostMapping("/uploadFiles")
-	public String uploadMultipleFiles(@RequestParam("files") MultipartFile[] files) {
+	@PostMapping("/uploadFirmFiles")
+	public String uploadMultipleFilesFirm(@RequestParam("files") MultipartFile[] files, Authentication login) {
 		for (MultipartFile file: files) {
-			imageStorageService.saveFile(file);
+			imageStorageService.saveFirmFile(file, login);
 			
 		}
 		return "redirect:/";
 	}
-	@GetMapping("/downloadFile/{fileId}")
-	public ResponseEntity<ByteArrayResource> downloadFile(@PathVariable Long fileId){
-		Images image = imageStorageService.getFile(fileId).get();
+	
+	@PostMapping("/uploadCategoryFiles")
+	public String uploadMultipleFilesCategory(@RequestParam("files") MultipartFile[] files, Authentication login, long categoryID) {
+		for (MultipartFile file: files) {
+			imageStorageService.saveCategoryFile(file, login, categoryID);
+			
+		}
+		return "redirect:/";
+	}
+	
+	@PostMapping("/uploadFirmFiles")
+	public String uploadMultipleFilesItem(@RequestParam("files") MultipartFile[] files, Authentication login, long itemID) {
+		for (MultipartFile file: files) {
+			imageStorageService.saveItemFile(file, login, itemID);
+			
+		}
+		return "redirect:/";
+	}
+	
+	@GetMapping("/downloadFirmFile/{fileId}")
+	public ResponseEntity<ByteArrayResource> downloadFirmFile(@PathVariable Long fileId){
+		FirmImage image = imageStorageService.getFirmFileByID(fileId).get();
 		return ResponseEntity.ok()
 				.contentType(MediaType.parseMediaType(image.getImageType()))
 				.header(HttpHeaders.CONTENT_DISPOSITION,"attachment:filename=\""+image.getImageName()+"\"")
 				.body(new ByteArrayResource(image.getData()));
 	}
 	
+	@GetMapping("/downloadCategoryFile/{fileId}")
+	public ResponseEntity<ByteArrayResource> downloadCategoryFile(@PathVariable Long fileId){
+		CategoryImage image = imageStorageService.getCategoryFileByID(fileId).get();
+		return ResponseEntity.ok()
+				.contentType(MediaType.parseMediaType(image.getImageType()))
+				.header(HttpHeaders.CONTENT_DISPOSITION,"attachment:filename=\""+image.getImageName()+"\"")
+				.body(new ByteArrayResource(image.getData()));
+	}
+	
+	@GetMapping("/downloadItemFile/{fileId}")
+	public ResponseEntity<ByteArrayResource> downloadItemFile(@PathVariable Long fileId){
+		ItemImage image = imageStorageService.getItemFileByID(fileId).get();
+		return ResponseEntity.ok()
+				.contentType(MediaType.parseMediaType(image.getImageType()))
+				.header(HttpHeaders.CONTENT_DISPOSITION,"attachment:filename=\""+image.getImageName()+"\"")
+				.body(new ByteArrayResource(image.getData()));
+	}
 }
