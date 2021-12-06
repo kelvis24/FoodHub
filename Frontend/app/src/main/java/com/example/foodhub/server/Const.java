3a01b5e8e0dee5adde1dff35c90e861c5e5d166a
kelvis24 <ekimara@iastate.edu>
2021-12-06 00:23:59 -0600

    fixed the home page textview size and used a temporary  online ws:// link

--- Frontend/app/src/main/java/com/example/foodhub/server/Const.java
+++ Frontend/app/src/main/java/com/example/foodhub/server/Const.java
@@ -20,20 +20,7 @@ public class Const {
      * A URL for the local chat server path
      */
     //ws://localhost:8025/myContextRoot
-    //public static final String CHAT_SERVER_PATH = " ws://demo.piesocket.com/v3/channel_1?api_key=oCdCMcMPQpbvNjUIzqtvF1d2X2okWpDQj4AwARJuAgtjhzKxVEjQU6IdCjwm&notify_self";
-
-
     public static final String CHAT_SERVER_PATH ="wss://demo.piesocket.com/v3/channel_1?api_key=oCdCMcMPQpbvNjUIzqtvF1d2X2okWpDQj4AwARJuAgtjhzKxVEjQU6IdCjwm&notify_self";
-
     //= "ws://coms-309-002.cs.iastate.edu:3000/OTC/675";
             //"ws://localhost:3000/OTC/2343";
-
-
-<<<<<<< HEAD
-    //http://coms-309-002.cs.iastate.edu:3000/debug-get-customers
-    //     ws://coms-309-002.cs.iastate.edu:3000/OTC/1232
-=======
-    public static final String OTHER_CHAT_SERVER_PATH = "ws://192.168.0.136:3000";
-
->>>>>>> c37506eb535347d6203f9e503d20c8594f3d8470
 }
